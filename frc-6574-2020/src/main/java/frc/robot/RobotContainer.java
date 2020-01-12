7cb7e36a898d1ce5ccca801e31966c4e621c78ca
Morgan Stillwagon <Stimo08@whippets.wwusd.org>
2020-01-12 16:27:14 -0600

    removed comment

--- frc-6574-2020/src/main/java/frc/robot/RobotContainer.java
+++ frc-6574-2020/src/main/java/frc/robot/RobotContainer.java
@@ -35,7 +35,6 @@ public class RobotContainer {
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-    // Configure the button bindings
     configureButtonBindings();
   }
 
