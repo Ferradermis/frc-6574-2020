3e0d453496d98f443c007809a3cf4a45b6530ae9
John Chenoweth <59900195+chenowej@users.noreply.github.com>
2020-02-08 18:02:36 -0600

    push for Feb 8

--- frc-6574-2020/src/main/java/frc/robot/subsystems/Shooter.java
+++ frc-6574-2020/src/main/java/frc/robot/subsystems/Shooter.java
@@ -25,10 +25,9 @@ public class Shooter extends SubsystemBase {
    * Creates a new Shooter.
    */
 
-  // CREATE and connect to motors
-  // shooter/spinner is two falcons -- built in encoder talonFX
-  private WPI_TalonFX spinner1 = new WPI_TalonFX(RobotMap.SPINNER1_CAN_ID);
-  private WPI_TalonFX spinner2 = new WPI_TalonFX(RobotMap.SPINNER2_CAN_ID);
+  // shooter/launcher is two falcons -- built in encoder talonFX
+  private WPI_TalonFX launcher1 = new WPI_TalonFX(RobotMap.SPINNER1_CAN_ID);
+  private WPI_TalonFX launcher2 = new WPI_TalonFX(RobotMap.SPINNER2_CAN_ID);
 
   // loader neo550's
   private CANSparkMax loader = new CANSparkMax(RobotMap.LOADER_CAN_ID, MotorType.kBrushless);
@@ -36,7 +35,7 @@ public class Shooter extends SubsystemBase {
   // rotator vexPro775
   private TalonSRX turretRotator = new TalonSRX(RobotMap.TURRET_CAN_ID);
   
- 
+  // hood controller for raising and lowering
   public DoubleSolenoid hoodController = new DoubleSolenoid(2, 3);
   // public Solenoid hoodController = new Solenoid(3);
 
@@ -47,54 +46,8 @@ public class Shooter extends SubsystemBase {
   private boolean shooting = false;
 
   public Shooter() {
-    // Set up motors
-    double rampRate = 0.2; //time in seconds to go from 0 to full throttle; 0.2 is selected on feel by drivers for 2019
- 
-    spinner2.follow(spinner1);
-    spinner1.configFactoryDefault();
-    spinner2.configFactoryDefault();
-    spinner1.configOpenloopRamp(rampRate);
-    spinner2.configOpenloopRamp(rampRate);
-    spinner1.setNeutralMode(NeutralMode.Coast); // MAKE SURE WE ARE IN COAST MODE
-    spinner2.setNeutralMode(NeutralMode.Coast); // MAKE SURE WE ARE IN COAST MODE
-
-    // SEE CODE FROM: https://github.com/CrossTheRoadElec/Phoenix-Examples-Languages/blob/master/Java/VelocityClosedLoop/src/main/java/frc/robot/Robot.java
-
-    /* Config sensor used for Primary PID [Velocity] */
-    spinner1.configSelectedFeedbackSensor(TalonFXFeedbackDevice.RemoteSensor0,
-    0, Constants.kTimeoutMs);
-
-    /**
-    * Phase sensor accordingly. 
-    * Positive Sensor Reading should match Green (blinking) Leds on Talon
-    */
-    spinner1.setSensorPhase(true);
-
-    /* Config the peak and nominal outputs */
-    spinner1.configNominalOutputForward(0, Constants.kTimeoutMs);
-    spinner1.configNominalOutputReverse(0, Constants.kTimeoutMs);
-    spinner1.configPeakOutputForward(1, Constants.kTimeoutMs);
-    spinner1.configPeakOutputReverse(-1, Constants.kTimeoutMs);
-
-    /* Config the Velocity closed loop gains in slot0 */
-    double kF = 1023.0/7200.0; // ??
-    double kP = 0.25;
-    double kI = 0.001;
-    double kD = 20;
-    spinner1.config_kF(Constants.kPIDLoopIdx, kF, Constants.kTimeoutMs);
-    spinner1.config_kP(Constants.kPIDLoopIdx, kP, Constants.kTimeoutMs);
-    spinner1.config_kI(Constants.kPIDLoopIdx, kI, Constants.kTimeoutMs);
-    spinner1.config_kD(Constants.kPIDLoopIdx, kD, Constants.kTimeoutMs);
-
-    int currentLimit = 30; 
- 
-    loader.setOpenLoopRampRate(rampRate);
-    loader.setSmartCurrentLimit(currentLimit);
-
-    //turns LED on
+    configureMotors();
     limelight.ledOn();
-
-    //Sets limelight to target powerport
     limelight.setTarget(0);
   }
 
@@ -104,7 +57,7 @@ public class Shooter extends SubsystemBase {
     if ((shooting == true)&&(limelight.hasTarget())) {
       spin(getDistanceToTarget());
       aim();
-      if (aimed() && spinnerReady(getDistanceToTarget())) {
+      if (aimed() && launcherReady(getDistanceToTarget())) {
           loadAndFire();
         } else { // shooting, but not aimed or not ready
           stopLoader();
@@ -129,42 +82,39 @@ public class Shooter extends SubsystemBase {
     } else {
       stopShooting();
     }
+  }
 
- }
+  private double getDistanceToTarget() {
+    //All calculations are in centimeters
+    final double h2 = 86.36; //height of target
+    final double h1 = 16.51; //height of camera
+    // NOTE in final code, just calculate h2 - h1 and set a variable    
+    final double A1 = 3.19; //Angle of camera relative to ground
 
-  private double getDistanceToTarget(){
-   //All calculations are in centimeters
-   final double h2 = 86.36; //height of target
-   final double h1 = 16.51; //height of camera
-   // NOTE in final code, just calculate h2 - h1 and set a variable    
-   final double A1 = 3.19; //Angle of camera relative to ground
-
-   double angleY = limelight.getAngleY();
+    double angleY = limelight.getAngleY();
     
-   // calculate currentDistance from target
-   return (h2-h1)/Math.tan((angleY+A1)*Math.PI/180);
-
+    // calculate currentDistance from target
+    return (h2-h1)/Math.tan((angleY+A1)*Math.PI/180);
   }
 
   private boolean aimed() {       
     final double tolerance = 0.5;
-    return (Math.abs(limelight.getAngleX())< tolerance);
+    return (Math.abs(limelight.getAngleX()) < tolerance);
   }
 
   private void spin(double distance) {
     // need to figure out this formula to set velocity based on distance
     // see the html file linked at the top of this java file
     double targetVelocity_UnitsPer100ms = distance * 500.0 * 4096 / 600;
-			/* 500 RPM in either direction */
-			spinner1.set(ControlMode.Velocity, targetVelocity_UnitsPer100ms);
-
+		/* 500 RPM in either direction */
+		launcher1.set(ControlMode.Velocity, targetVelocity_UnitsPer100ms);
   }
 
-  private boolean spinnerReady(double distance) {
+  private boolean launcherReady(double distance) {
     // see if selected sensor velocity is within tolerance of the speed we want
     double something = .9999; // need to figure out what this should be
     double intendedVelocity = distance * something;
-    return (spinner1.getSelectedSensorVelocity() >= intendedVelocity);
+    return (launcher1.getSelectedSensorVelocity() >= intendedVelocity);
   }
 
   private void loadAndFire()
@@ -179,11 +129,12 @@ public class Shooter extends SubsystemBase {
   public void stopShooting() {
     shooting = false;
     stopAiming();
-    stopSpinner();
+    stopLauncher();
     stopLoader();
   }
-  private void stopSpinner() {
-    spinner1.set(ControlMode.PercentOutput, 0);
+
+  private void stopLauncher() {
+    launcher1.set(ControlMode.PercentOutput, 0);
   }
 
   private void stopAiming() {
@@ -202,7 +153,52 @@ public class Shooter extends SubsystemBase {
 
   public void lowerHood()
   {
-    hoodController.set(DoubleSolenoid.Value.kForward);
+    hoodController.set(DoubleSolenoid.Value.kReverse);
   }
 
+  private void configureMotors(){
+    // Set up motors
+    double rampRate = 0.2; //time in seconds to go from 0 to full throttle; 0.2 is selected on feel by drivers for 2019
+ 
+    launcher2.follow(launcher1);
+    launcher1.configFactoryDefault();
+    launcher2.configFactoryDefault();
+    launcher1.configOpenloopRamp(rampRate);
+    launcher2.configOpenloopRamp(rampRate);
+    launcher1.setNeutralMode(NeutralMode.Coast); // MAKE SURE WE ARE IN COAST MODE
+    launcher2.setNeutralMode(NeutralMode.Coast); // MAKE SURE WE ARE IN COAST MODE
+
+    // SEE CODE FROM: https://github.com/CrossTheRoadElec/Phoenix-Examples-Languages/blob/master/Java/VelocityClosedLoop/src/main/java/frc/robot/Robot.java
+
+    /* Config sensor used for Primary PID [Velocity] */
+    launcher1.configSelectedFeedbackSensor(TalonFXFeedbackDevice.RemoteSensor0,
+     0, Constants.kTimeoutMs);
+
+    /**
+    * Phase sensor accordingly. 
+    * Positive Sensor Reading should match Green (blinking) Leds on Talon
+    */
+    launcher1.setSensorPhase(true);
+
+    /* Config the peak and nominal outputs */
+    launcher1.configNominalOutputForward(0, Constants.kTimeoutMs);
+    launcher1.configNominalOutputReverse(0, Constants.kTimeoutMs);
+    launcher1.configPeakOutputForward(1, Constants.kTimeoutMs);
+    launcher1.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+
+    /* Config the Velocity closed loop gains in slot0 */
+    double kF = 1023.0/7200.0; // ??
+    double kP = 0.25;
+    double kI = 0.001;
+    double kD = 20;
+    launcher1.config_kF(Constants.kPIDLoopIdx, kF, Constants.kTimeoutMs);
+    launcher1.config_kP(Constants.kPIDLoopIdx, kP, Constants.kTimeoutMs);
+    launcher1.config_kI(Constants.kPIDLoopIdx, kI, Constants.kTimeoutMs);
+    launcher1.config_kD(Constants.kPIDLoopIdx, kD, Constants.kTimeoutMs);
+
+    int currentLimit = 30; 
+
+    loader.setOpenLoopRampRate(rampRate);
+    loader.setSmartCurrentLimit(currentLimit);
+  }
 }
